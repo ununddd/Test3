db28dabe49788047a4f588c7f859174f383a3406
un143 <chengmotoral@gmail.com>
2017-01-10 11:39:27 +0800

    local_second

---
+++ QuranApplication.java
@@ -0,0 +1,72 @@
+package com.quran.labs.androidquran;
+
+//import com.crashlytics.android.Crashlytics;
+import com.quran.labs.androidquran.component.ApplicationComponent;
+import com.quran.labs.androidquran.component.DaggerApplicationComponent;
+import com.quran.labs.androidquran.module.ApplicationModule;
+import com.quran.labs.androidquran.util.QuranSettings;
+import com.quran.labs.androidquran.util.RecordingLogTree;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.support.annotation.NonNull;
+
+import java.util.Locale;
+
+//import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
+
+public class QuranApplication extends Application {
+  private ApplicationComponent applicationComponent;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    //Fabric.with(this, new Crashlytics());
+    Timber.plant(new RecordingLogTree());
+    initializeInjector();
+  }
+
+  private void initializeInjector() {
+    this.applicationComponent = DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this))
+        .build();
+  }
+
+  public ApplicationComponent getApplicationComponent() {
+    return this.applicationComponent;
+  }
+
+  public void refreshLocale(@NonNull Context context, boolean force) {
+    final String language = QuranSettings.getInstance(this).isArabicNames() ? "ar" : null;
+
+    final Locale locale;
+    if ("ar".equals(language)) {
+      locale = new Locale("ar");
+    } else if (force) {
+      // get the system locale (since we overwrote the default locale)
+      locale = Resources.getSystem().getConfiguration().locale;
+    } else {
+      // nothing to do...
+      return;
+    }
+
+    updateLocale(context, locale);
+    final Context appContext = context.getApplicationContext();
+    if (context != appContext) {
+      updateLocale(appContext, locale);
+    }
+  }
+
+  private void updateLocale(@NonNull Context context, @NonNull Locale locale) {
+    final Resources resources = context.getResources();
+    Configuration config = resources.getConfiguration();
+    config.locale = locale;
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+      config.setLayoutDirection(config.locale);
+    }
+    resources.updateConfiguration(config, resources.getDisplayMetrics());
+  }
+}
